b9702bee4f78082f15d51c7af0630409af5468c6
esilean <le.bevilaqua@gmail.com>
2020-05-15 14:44:51 -0300

    fix env config for prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,62 +1,47 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
-const dotenv = require('dotenv')
-const webpack = require('webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = () => {
-
-    const env = dotenv.config().parsed
-    const envKeys = Object.keys(env).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(env[next])
-        return prev
-    }, {})
-
-    return {
-        entry: './src/index.tsx',
-        output: {
-            path: path.join(__dirname, '/dist'),
-            filename: '[name].bundle.[hash].js',
-            publicPath: '/'
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: '[name].bundle.[hash].js',
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: ['ts-loader', 'eslint-loader'],
         },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js']
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader',
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './public/index.html'
-            }),
-            new webpack.DefinePlugin(envKeys),
-            new CleanWebpackPlugin(),
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.ts(x?)$/,
-                    exclude: /node_modules/,
-                    use: ['ts-loader', 'eslint-loader']
-                },
-                {
-                    enforce: 'pre',
-                    test: /\.js$/,
-                    loader: 'source-map-loader'
-                },
-                {
-                    test: /\.(less|css)$/,
-                    use: [
-                        'style-loader',
-                        'css-loader',
-                        'less-loader'
-                    ]
-                },
-                {
-                    test: /\.(png|jpe?g|gif)$/i,
-                    use: ['file-loader']
-
-                }
-            ]
-        }
-    }
+        {
+          test: /\.(less|css)$/,
+          use: ['style-loader', 'css-loader', 'less-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: ['file-loader'],
+        },
+      ],
+    },
+  }
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,24 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('webpack')
+const dotenv = require('dotenv')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
-module.exports = merge(common(), {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 8080,
-    historyApiFallback: true,
-  },
-})
+module.exports = () => {
+  const env = dotenv.config().parsed
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next])
+    return prev
+  }, {})
+
+  return merge(common(), {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      port: 8080,
+      historyApiFallback: true,
+    },
+    plugins: [new webpack.DefinePlugin(envKeys)],
+  })
+}
